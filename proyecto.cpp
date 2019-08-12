7f3e49256951b779b57c29f0b061e38c73ff41cd
emyesme <emily.carvajal@correounivalle.edu.co>
2019-08-12 00:34:54 -0500

    grafica costo en 2 m en intervalo

--- proyecto.cpp
+++ proyecto.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <random>
 #include <vector>
+#include <sstream>
 #include "matplotlibcpp.h"
 
 using namespace std;
@@ -25,25 +26,69 @@ struct Car {
 	}
 };
 
+void carsGenerator(int m,double &sum,int start, int years,int &failCars, double &maxtime, double &mintime, vector<Car> &myCars, vector<double> &yline);
+void carsTimeDamage(int m, int years, vector<Car> myCars, vector<double> &yline);
+void carsHistogramDamage(int maxtime, int mintime, int widthInterval, vector<Car> myCars);
+void carsFailGood(int m, int start,int years,vector <Car> &myCars,vector <double> &yline, vector <int> &compare);
+void carsCostInterval(int m1, int m2, int start,int years,vector <Car> &myCars,vector <double> &yline, vector <int> &compare);
+
+
+int main(){
+	double sum = 0;
+	int failCars = 0;
+	double maxtime = 0;
+	double mintime = 9999;
+	int years = 100;
+	
+	//cout << "el costo inicial es "<< 5+4*m+0.2*m*m << endl;
+
+	vector<Car> myCars;
+	vector<double> yline (years*10);
+	vector <int> compare;
+	
+	carsGenerator(4,sum,0,100,failCars,maxtime,mintime,myCars,yline);
+	/*Grafica de carros versus averia en años */
+	//carsTimeDamage(m, years, myCars, yline);
+
+	/*Grafica de tabla de frecuencias */
+	//carsHistogramDamage(maxtime, mintime, 1, myCars);
+	
+	/*total vs fail 1000-2000 m=4 */
+	//carsFailGood(4, 1000, 200, myCars, yline, compare);
+
+	/*total vs fail 1000-2000 6 */
+	//carsFailGood(6, 1000, 200, myCars, yline, compare);
+
+	/*Grafica cost m=4 y cost m = 6 0-1000 */
+	//carsCostInterval(4,6,0,100,myCars,yline,compare);
+
+	/*Grafica cost m=4 y cost m = 6 1000-2000 */
+	//carsCostInterval(4,6,1000,200,myCars,yline,compare);
+
+	/*Grafica cost m=4 y cost m = 6 2000-3000 */
+	carsCostInterval(4,6,2000,300,myCars,yline,compare);
+
+	return 0;
+}
+
 void carsGenerator(int m,double &sum,int start, int years,int &failCars, double &maxtime, double &mintime, vector<Car> &myCars, vector<double> &yline){
 	Car * currentCar = NULL;
-	cout << m << endl;
 	double lambda = (double) 1/m;
 	//Generador de números aleatorios
 	default_random_engine generator;
 	//Distribución exponencial
 	exponential_distribution<double> distribution(lambda);
 	int cars = 0;
-	int trash = 0;
+	int valid = 0;
 	for(int i=1; i<=years; i++){
 		for (int j=1; j<=10; j++){
 			if(cars < start){
-				trash++;
 				currentCar = new Car(m, distribution, generator);
 				currentCar->timeForFail = 0;
 				currentCar->cost = 0;
 				myCars.insert(myCars.begin(),*currentCar);
 			}else{
+				valid++;
 				currentCar = new Car(m, distribution, generator);
 				myCars.insert(myCars.begin(),*currentCar);
 				if(currentCar->timeForFail <= 4){
@@ -58,39 +103,14 @@ void carsGenerator(int m,double &sum,int start, int years,int &failCars, double 
 					mintime = myCars.begin()->timeForFail;
 				}
 				//The index of actual car
-				int nCar = (i-1)*10+j;
-				sum = (sum * (nCar-1) + currentCar->cost)/nCar;
+				sum = (sum * (valid-1) + currentCar->cost)/valid;
 			}
 			cars++;
 		}
 	}
-	cout << "\ncars " << cars << endl;
-	cout << "trash "  << trash << endl;
 }
 
-
-
-int main(){
-	double sum = 0;
-	int start = 0;
-	int widthInterval = 1;
-	double maxtime = 0;
-	double mintime = 9999;	
-	int years = 100;
-	int m = 4;
-	int failCars = 0;
-	cout << "el costo inicial es "<< 5+4*m+0.2*m*m << endl;
-	vector<Car> myCars;
-	vector<double> yline (years*10);
-	vector <int> compare (2);
-	int versus [2] = {0,0};
-	//////////////////////////////////////////////////////
-	carsGenerator(m,sum,start,years,failCars,maxtime,mintime,myCars,yline);
-	//////////////////////////////////////////////////////
-	cout << "EL COSTO PROMEDIO POR CARRO ES: " << sum << endl;
-	cout << "El número de carros dañados fue: " << failCars << endl;
-	//tiempo en que se dañaron los autos con linea de garantia
-	/*
+void carsTimeDamage(int m, int years, vector<Car> myCars, vector<double> &yline){
 	vector <double> myTime (years*10);
 	for(vector<Car>::iterator it=myCars.begin(); it != myCars.end(); it++){
 		myTime.insert(myTime.begin(),it->timeForFail);
@@ -105,9 +125,9 @@ int main(){
 	plt::title("Generando fallas");
 	plt::show();
 	plt::clf();
- 	*/
-	//frecuencias acumuladas para ver exponencial
-	/*
+}
+
+void carsHistogramDamage(int maxtime, int mintime, int widthInterval, vector<Car> myCars){
 	int diffTime = maxtime - mintime;
 	int numIntervals = diffTime / widthInterval;
 	int freqIntervals [numIntervals];
@@ -124,109 +144,78 @@ int main(){
 	plt::xlim(0,numIntervals);
 	plt::bar(histogram);
 	plt::show();
-	*/
-
-	//tabla de frecuencias
-	/*
-	cout << "frecuencias" << endl;
-	cout << "intervalos: " << numIntervals << endl;
-	for(int j = 0; j<numIntervals; j++){
-		cout << j << ","<< j+1 <<" | "<< freqIntervals[j] << endl;
-	}*/
-
-	//total vs fail 0-1000
-	
-	versus[0] = years*10 - start - failCars;
-	versus[1] = failCars;
-	compare.insert(compare.begin(), begin(versus),end(versus));
 	plt::clf();
-	plt::bar(compare);
-	plt::title("sin Fallos vs con Fallos m=4");
-	//plt::save("0-1000m4.png");
-	plt::clf();
-	
-	
-	//total vs fail 1000-2000 m=4
-	sum = 0;
-	start = 1000;
-	widthInterval = 1;
-	maxtime = 0;
-	mintime = 9999;	
-	years = 200;
-	m = 4;
-	failCars = 0;
+}
+
+void carsCostInterval(int m1, int m2, int start,int years,vector <Car> &myCars,vector <double> &yline, vector <int> &compare){
+	double sum = 0;
+	int failCars = 0;
+	double maxtime = 0;
+	double mintime = 9999;
 	myCars.clear();
 	yline.clear();
 	compare.clear();
-	//////////////////////////////////////////////////////
-	carsGenerator(m,sum,start,years,failCars,maxtime,mintime,myCars,yline);
-	//////////////////////////////////////////////////////
-	cout << "EL COSTO PROMEDIO POR CARRO ES: " << sum << endl;
-	cout << "El número de carros dañados fue: " << failCars << endl;
-	versus[0] = years*10 - start - failCars;
-	versus[1] = failCars;
-	compare.insert(compare.begin(), begin(versus),end(versus));
-	cout << "failcars " << failCars << endl;
+
+	carsGenerator(m1,sum,start,years,failCars,maxtime,mintime,myCars,yline);
+	cout << "m " << m1 <<" EL COSTO PROMEDIO POR CARRO ES: " << sum << endl;
+	compare.push_back(sum);
+
+	sum = 0;
+	failCars = 0;
+	maxtime = 0;
+	mintime = 9999;
+	myCars.clear();
+	yline.clear();
+
+	carsGenerator(m2,sum,start,years,failCars,maxtime,mintime,myCars,yline);
+	cout << "m " << m2 << " EL COSTO PROMEDIO POR CARRO ES: " << sum << endl;	
+	compare.push_back(sum);
+
+	vector <string> labels (2);
+	vector <int> ticks (2);
+
+	labels.insert(labels.begin(),"m=4");
+	labels.insert(labels.begin(),"m=6");
+
+	ticks.insert(ticks.begin(),0);
+	ticks.insert(ticks.begin(),1);
+
+	plt::clf();
 	plt::bar(compare);
-	plt::title("sin Fallos vs con Fallos m=4");
-	//plt::save("1000-2000m4.png");
+	plt::xticks(ticks,labels);
+	plt::title("Costo");
+	plt::show();
 	plt::clf();
 
-	//total vs fail 0-1000 6
-	sum = 0;
-	start = 0;
-	widthInterval = 1;
-	maxtime = 0;
-	mintime = 9999;	
-	years = 100;
-	m = 6;
-	failCars = 0;
+}
+
+void carsFailGood(int m, int start,int years,vector <Car> &myCars,vector <double> &yline, vector <int> &compare){
+	double sum = 0;
+	int failCars = 0;
+	double maxtime = 0;
+	double mintime = 9999;
+	int versus [2] = {0,0};
 	myCars.clear();
 	yline.clear();
 	compare.clear();
-	//////////////////////////////////////////////////////
-	carsGenerator(m,sum,start,years,failCars,maxtime,mintime,myCars,yline);
-	//////////////////////////////////////////////////////
-	cout << "EL COSTO PROMEDIO POR CARRO ES: " << sum << endl;
-	cout << "El número de carros dañados fue: " << failCars << endl;
+
+	carsGenerator(m,sum,start,years,failCars,maxtime,mintime,myCars,yline);	
+
 	versus[0] = years*10 - start - failCars;
 	versus[1] = failCars;
 	compare.insert(compare.begin(), begin(versus),end(versus));
-	cout << "failcars " << failCars << endl;
+
 	plt::clf();
 	plt::bar(compare);
-	plt::title("sin Fallos vs con Fallos m=6");
-	//plt::save("0-1000m6.png");
- 	plt::clf();
-
-
-	//total vs fail 1000-2000 6
-	sum = 0;
-	start = 1000;
-	widthInterval = 1;
-	maxtime = 0;
-	mintime = 9999;	
-	years = 200;
-	m = 6;
-	failCars = 0;
-	myCars.clear();
-	yline.clear();
-	compare.clear();
-	//////////////////////////////////////////////////////
-	carsGenerator(m,sum,start,years,failCars,maxtime,mintime,myCars,yline);
-	//////////////////////////////////////////////////////
-	cout << "EL COSTO PROMEDIO POR CARRO ES: " << sum << endl;
-	cout << "El número de carros dañados fue: " << failCars << endl;
-	versus[0] = years*10 - start - failCars;
-	versus[1] = failCars;
-	compare.insert(compare.begin(), begin(versus),end(versus));
-	cout << "failcars " << failCars << endl;
+	plt::title("sin Fallos vs con Fallos");
+	std::string out_1, out_2, out_3;
+	std::stringstream ss1, ss2, ss3;
+	ss1 << years*10;
+	ss2 << m;
+	ss3 << start;
+	out_1 = ss1.str();
+	out_2 = ss2.str();
+	out_3 = ss3.str();
+	plt::save(out_3+"-"+out_1+"m"+out_2+".png");
 	plt::clf();
-	plt::bar(compare);
-	plt::title("sin Fallos vs con Fallos m=6");
-	//plt::save("1000-2000m6.png");
- 	plt::clf();
-
-	
-	return 0;
 }
